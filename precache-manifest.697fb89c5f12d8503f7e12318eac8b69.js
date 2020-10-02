99b0b85568da37a1a1fdb851c8749f2d2384f5a4
Ramu Chinta <Ramu.Chinta@ril.com>
2020-10-02 12:59:54 +0530

    Updates

---
+++ precache-manifest.697fb89c5f12d8503f7e12318eac8b69.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc1223ef2d1cb2ec69630e3da15e6fc",
+    "url": "/onlineshopdemo/index.html"
+  },
+  {
+    "revision": "e688155ed83f0b3af12e",
+    "url": "/onlineshopdemo/static/css/5.92710b69.chunk.css"
+  },
+  {
+    "revision": "056b9de00ebd097a0d82",
+    "url": "/onlineshopdemo/static/css/6.16d8664e.chunk.css"
+  },
+  {
+    "revision": "6f383bc7b59da67f5639",
+    "url": "/onlineshopdemo/static/css/main.1dd1c60b.chunk.css"
+  },
+  {
+    "revision": "846a56a351aad0140a00",
+    "url": "/onlineshopdemo/static/js/0.8f5d307b.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/onlineshopdemo/static/js/0.8f5d307b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd84594ac498353d97a3",
+    "url": "/onlineshopdemo/static/js/3.09e9266e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/onlineshopdemo/static/js/3.09e9266e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee049776ee37c5888b5",
+    "url": "/onlineshopdemo/static/js/4.71a444fa.chunk.js"
+  },
+  {
+    "revision": "e688155ed83f0b3af12e",
+    "url": "/onlineshopdemo/static/js/5.ec55e96b.chunk.js"
+  },
+  {
+    "revision": "056b9de00ebd097a0d82",
+    "url": "/onlineshopdemo/static/js/6.166845c4.chunk.js"
+  },
+  {
+    "revision": "6f383bc7b59da67f5639",
+    "url": "/onlineshopdemo/static/js/main.c8fea2b3.chunk.js"
+  },
+  {
+    "revision": "e899d8daae5369687b86",
+    "url": "/onlineshopdemo/static/js/runtime-main.f9bcb027.js"
+  }
+]);
